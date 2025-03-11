4a99e3f358104fff14d51461c0b9ee86993a7571
aikhe <ikeandrie.ro@gmail.com>
2025-03-11 18:49:33 +0800

    feat(components): icons component & katha icon

--- web/components/Containers/NavBar/index.tsx
+++ web/components/Containers/NavBar/index.tsx
@@ -1,13 +1,14 @@
 import WithKathaLogo from "@/components/withKathaLogo";
+import Link from "next/link";
 import type { FC } from "react";
 
 const NavBar: FC = () => {
   return (
-    <header className="h-[60px] w-full z-50 border-b-2 bg-card fixed">
+    <nav className="h-[60px] w-full z-50 border-b-2 bg-card fixed">
       <div className="mx-16 px-6 h-full flex items-center">
         <WithKathaLogo />
       </div>
-    </header>
+    </nav>
   );
 };
 
--- web/components/withKathaLogo.tsx
+++ web/components/withKathaLogo.tsx
@@ -1,6 +1,13 @@
 import type { FC } from "react";
 import KathaLogo from "./Common/KathaLogo";
+import Link from "next/link";
 
-const WithKathaLogo: FC = () => <KathaLogo />;
+const WithKathaLogo: FC = () => {
+  return (
+    <Link href="/">
+      <KathaLogo />
+    </Link>
+  );
+};
 
 export default WithKathaLogo;
